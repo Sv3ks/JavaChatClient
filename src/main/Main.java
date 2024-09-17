a838f99f165d005b9678546671721077f58cb0f2
Sv3ks <snorre@silkjaer.dk>
2024-09-17 16:50:26 +0200

    logging

--- src/main/Main.java
+++ src/main/Main.java
@@ -26,6 +26,7 @@ public class Main {
 				System.out.println("Enter room port:");
 				int port = Integer.parseInt(System.console().readLine());
 				Server server = new Server(port);
+				server.configureLogging(true, "log.txt");
 				server.run();
 			}
 			
--- src/main/Server.java
+++ src/main/Server.java
@@ -1,12 +1,16 @@
 package main;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,20 +18,46 @@ public class Server implements Runnable {
 	private static List<ClientHandler> clients = new ArrayList<>();
 	private final int port;
 	private ServerSocket s;
+	private boolean logging = false;
+	private FileWriter logger;
 	
 	public Server(int port) {
 		this.port = port;
 	}
 
+	public void configureLogging(boolean logging, String logPath) {
+		this.logging = logging;
+		if(logging) {
+			try {
+				new File(logPath).createNewFile();
+				logger = new FileWriter(logPath);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private String log(String message) {
+		if(logging) {
+			try {
+				logger.write("["+LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"))+"] "+message+"\n");
+				logger.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return message;
+	}
+	
 	@Override
 	public void run() {
 		try {
 			s = new ServerSocket(port);
-			System.out.println("Room opened at " + InetAddress.getLocalHost().getHostAddress() + ":" + port + ".");
+			System.out.println(log("Room opened at " + InetAddress.getLocalHost().getHostAddress() + ":" + port + "."));
 			
 			while (true) {
 				Socket client = s.accept();
-				System.out.println("User joined at " + s.getInetAddress().getHostAddress());
+				System.out.println(log("User joined at " + s.getInetAddress().getHostAddress()));
 				for (ClientHandler cHandler : clients) {
 					cHandler.send(s.getInetAddress().getHostAddress()+" joined the room.");
 				}
@@ -76,7 +106,7 @@ public class Server implements Runnable {
 					} else {
 						message = "[" + name + "]: " + input;
 					}
-					System.out.println(message);
+					System.out.println(log(message));
 					
 					for (ClientHandler handler : clients) {
 						if(handler!=this) {
@@ -86,7 +116,7 @@ public class Server implements Runnable {
 				}
 			} catch (IOException e) {
 				//e.printStackTrace();
-				System.out.println(name+" disconnected.");
+				System.out.println(log(name+" disconnected."));
 				clients.remove(this);
 				for (ClientHandler handler : clients) {
 					handler.send(name+" disconnected.");
